7ae8d3031600da70b29d5d2ec7887f64b2dd7156
lukasz.jablonski <lukasz.jablonski@S50-14>
2024-10-04 09:52:03 +0200

    zapis z konca artow

--- pesel/Program.cs
+++ pesel/Program.cs
@@ -2,6 +2,12 @@
 {
     internal class Program
     {
+
+        public static char PeselGender(string pesel)
+        {
+            return;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
