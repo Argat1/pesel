5096e1bfc75b3fcd5468d31f1ade8eb4b30e23b7
lukasz.jablonski <lukasz.jablonski@S50-14>
2024-10-04 09:45:22 +0200

    addedPerson _class

---
+++ pesel/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pesel
+{
+    internal class Person
+    {
+        int Id;
+        int age;
+        string name;
+        string surname;
+        string gender;
+        string pesel;
+
+        Person(int id, int age, string name, string surname, string gender, string pesel)
+        {
+            Id = id;
+            this.age = age;
+            this.name = name;
+            this.surname = surname;
+            this.gender = gender;
+            this.pesel = pesel;
+        }
+
+        Person()
+        {
+
+        }
+    }
+}
